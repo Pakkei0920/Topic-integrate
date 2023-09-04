42608323a713479a23a889c3dbd99a8817633707
Pak Kei <106027537+Pakkei0920@users.noreply.github.com>
2023-09-05 04:29:28 +0800

    Add files via upload

---
+++ 第二部分/Version2-SEP04/updateCountdown.ino
@@ -0,0 +1,35 @@
+void updateCountdown() {
+  // 计算剩余时间
+  unsigned long currentTime = millis();
+  unsigned long recordingTimeElapsed = currentTime - recordingStartTime;
+  unsigned long recordingTimeRemaining = (recordingTimeElapsed >= RECORD_TIME * 1000 - 2000) ? 0 : ((RECORD_TIME * 1000 - 2000) - recordingTimeElapsed);
+
+
+if (canrecording == false && endrecording == false) {
+
+  Serial.print("主板時間:");
+  Serial.print(currentTime);
+  Serial.print("  錄音時間:");
+  Serial.println(recordingTimeRemaining);
+  
+  // 在OLED上显示倒计时
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("Recording...");
+  display.setCursor(0, 16);
+  display.print("Time Remaining: ");
+  display.print(recordingTimeRemaining / 1000); // 显示剩余秒数
+  display.display();
+  delay(500);
+  }
+else {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("No Record");
+  display.display();
+  delay(1000);
+}}
