2a1ae6b40d5a64ced33116f05b5c89267a083122
Pak Kei <106027537+Pakkei0920@users.noreply.github.com>
2023-12-03 21:12:56 +0800

    Add files via upload

---
+++ 第二部分-播放/Version6-NOV23/audio.ino
@@ -0,0 +1,21 @@
+void audio123(){
+  if (millis() % 2000 == 0) {
+   HTTPClient http;
+   http.begin("http://topic.paki91.com/LTE/audio.php");  // 替換成您的網頁 URL
+   
+  
+   if (http.GET() == HTTP_CODE_OK) {
+   String audio_id2 = http.getString();
+   http.end();
+   //比較
+   Serial.print("audio_id1：");Serial.print(audio_id1);
+   Serial.print(" | audio_id2：");Serial.println(audio_id2);
+   
+    if (audio_id1 != audio_id2) {
+       Serial.println("音頻檔案更新，正在接受");
+       audio.stopSong();
+       Serial.println(audio_id2);
+       audio.connecttohost(audio_id2.c_str());
+       audio_id1 = audio_id2;
+       Serial.println("Ending");
+    }}}}
